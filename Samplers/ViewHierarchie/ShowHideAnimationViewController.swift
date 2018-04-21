2d63eca923b3141bc0554b776ae71d507e555163
Toshiyuki Masuda <ochimasu@gmail.com>
2018-04-21 14:38:33 +0900

    Segmentでアニメーションを切り替えられる仕組みを作っておく

--- Samplers/ViewHierarchie/ShowHideAnimationViewController.swift
+++ Samplers/ViewHierarchie/ShowHideAnimationViewController.swift
@@ -10,6 +10,16 @@ import UIKit
 
 class ShowHideAnimationViewController: UIViewController {
 
+    private enum AnimationType: Int {
+        case normal = 0
+
+        func name() -> String {
+            switch self {
+            case .normal: return "normal"
+            }
+        }
+    }
+
     // MARK: - LifeCycle
 
     override func viewDidLoad() {
@@ -22,6 +32,15 @@ class ShowHideAnimationViewController: UIViewController {
 
     @IBOutlet private weak var targetButton: UIButton!
     @IBOutlet private weak var statusLabel: UILabel!
+    @IBOutlet private weak var animationSegment: UISegmentedControl! {
+        didSet {
+            animationSegment.removeAllSegments()
+            let type = AnimationType.normal
+            animationSegment.insertSegment(withTitle: type.name(), at: type.rawValue, animated: false)
+
+            animationSegment.selectedSegmentIndex = AnimationType.normal.rawValue
+        }
+    }
 
     private var hiddenStatus: Bool = false {
         didSet {
@@ -35,6 +54,23 @@ class ShowHideAnimationViewController: UIViewController {
         guard hiddenStatus else { return }
         hiddenStatus = false
 
+        switch AnimationType(rawValue: animationSegment.selectedSegmentIndex)! {
+        case .normal:
+            normalShow()
+        }
+    }
+
+    private func hideAnimation() {
+        guard !hiddenStatus else { return }
+        hiddenStatus = true
+
+        switch AnimationType(rawValue: animationSegment.selectedSegmentIndex)! {
+        case .normal:
+            normalHide()
+        }
+    }
+
+    private func normalShow() {
         targetButton.isHidden = false
 
         UIView.animate(withDuration: 1, delay: 0, options: [.beginFromCurrentState], animations: {
@@ -42,10 +78,7 @@ class ShowHideAnimationViewController: UIViewController {
         }, completion: nil)
     }
 
-    private func hideAnimation() {
-        guard !hiddenStatus else { return }
-        hiddenStatus = true
-
+    private func normalHide() {
         UIView.animate(withDuration: 1, delay: 0, options: [.beginFromCurrentState], animations: {
             self.targetButton.alpha = 0
         }) { (finished) in

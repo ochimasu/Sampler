fc23fd9836f125728a8e30b816ebccd6b2f0e581
Toshiyuki Masuda <ochimasu@gmail.com>
2018-04-21 11:02:51 +0900

    とりあえずリスト表示

---
+++ Samplers/ViewHierarchie/TableViewController.swift
@@ -0,0 +1,28 @@
+//
+//  TableViewController.swift
+//  Samplers
+//
+//  Created by Toshiyuki M on 2018/04/21.
+//  Copyright © 2018年 ochimasu. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "identifier", for: indexPath)
+        cell.textLabel?.text = "hoge"
+
+        return cell
+    }
+}
